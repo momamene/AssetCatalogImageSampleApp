02b01797afa793ddb3cbe00ef64013854b6a8682
momamene <momamene@gmail.com>
2019-09-23 17:46:13 +0900

    Simulate CustomBundle not loading under iOS 13 problem

---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '11.2'
+
+use_frameworks!
+
+target 'SampleLayoutApp' do
+  pod 'Layout'
+end
---
+++ Pods/Target Support Files/Pods-SampleLayoutApp/Pods-SampleLayoutApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SampleLayoutApp : NSObject
+@end
+@implementation PodsDummy_Pods_SampleLayoutApp
+@end
