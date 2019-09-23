b66948e6fd603166268cb99531c2c7974675155d
momamene <momamene@gmail.com>
2019-09-23 17:24:57 +0900

    Initial Commit

---
+++ SampleLayoutApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleLayoutApp
+//
+//  Created by Hyuntak Joo on 23/09/2019.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
